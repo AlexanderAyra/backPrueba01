baabfc559c002eedb3629e3422d155e929029286
Alexander Ayra <alexander.ayras@gmail.com>
2024-05-27 16:13:22 -0500

    Prueba01 back

---
+++ src/model/server.model.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+class Server {
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log('Corriendo en el puerto ' + this.port);
+    })
+  }
+
+}
+
+module.exports = Server;
